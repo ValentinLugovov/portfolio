da9db53ee84cdd08d96c9a77b058a4b664d2bae7
ValentinLugovov <valentinlugovov@gmail.com>
2022-08-26 20:17:01 +0300

    Filling component

--- portfolio/src/components/Hero/Hero.jsx
+++ portfolio/src/components/Hero/Hero.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import s from "./hero.module.css";
+import avatar from "../img/avatar.png";
 
 const Hero = (props) => {
-  return <div className={s.hero}></div>;
+  return (
+    <div id="hero" className={s.hero}>
+      <div className={s.one}>
+        Hi, I Am <span>Valentin Lugovov</span>👋
+      </div>
+      <div className={s.two}>Junior Front-End Developer</div>
+      <div className={s.three}>
+        I design and code beautifully simple things, and I love what I do.
+      </div>
+      <div className={s.four}>
+        <img src={avatar}></img>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
