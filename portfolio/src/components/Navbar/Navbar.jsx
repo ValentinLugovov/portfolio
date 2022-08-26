c789624fe028caa5538d98d7a5b809349c0ae5b3
ValentinLugovov <valentinlugovov@gmail.com>
2022-08-26 20:28:38 +0300

    Adds link in components for fast travel

--- portfolio/src/components/Navbar/Navbar.jsx
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -8,13 +8,21 @@ const Navbar = (props) => {
       <div className={s.box1}>
         <img src={logo}></img>
       </div>
-      <div className={s.box2}>Home</div>
-      <div className={s.box3}>About</div>
+      <div className={s.box2}>
+        <a href="#hero">Home</a>
+      </div>
+      <div className={s.box3}>
+        <a href="#about">About</a>
+      </div>
       <div className={s.box4}>
         <a href="#skills">Skills</a>
       </div>
-      <div className={s.box5}>Portfolio</div>
-      <div className={s.box6}>Contact</div>
+      <div className={s.box5}>
+        <a href="#project">Portfolio</a>
+      </div>
+      <div className={s.box6}>
+        <a href="#contact">Contact</a>
+      </div>
       <div className={s.box7}>
         <button className={s.btn}>Hire Me</button>
       </div>
