7fb60b4686726df87f2a88004f684cbcd5d18bc3
ValentinLugovov <valentinlugovov@gmail.com>
2022-08-26 23:02:27 +0300

    Moved all files to the main folder for deploy

--- portfolio/src/components/Contacts/Contacts.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import s from "./contacts.module.css";
-
-const Contacts = (props) => {
-  return <div id="contact" className={s.contacts}></div>;
-};
-
-export default Contacts;
--- portfolio/src/components/Projects/Projects.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import s from "./projects.module.css";
-
-const Projects = (props) => {
-  return <div id="project" className={s.projects}></div>;
-};
-
-export default Projects;
