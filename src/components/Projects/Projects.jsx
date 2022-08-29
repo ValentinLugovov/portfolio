92aae41142570e7ed92fd863d41e4b66aacdf184
ValentinLugovov <valentinlugovov@gmail.com>
2022-08-29 18:47:18 +0300

    Adds tabs

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,35 +1,36 @@
 import React from "react";
 import Parallax from "./Parallax/Parallax";
 import s from "./projects.module.css";
+// import Site from "./Site";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "./react-tabs.css";
 
 const Projects = (props) => {
   return (
     <div id="project" className={s.projects}>
       <div className={s.note}>My Works</div>
       <div className={s.header}>Featured Works</div>
-      <div className={s.panel}>
-        <div className={s.box}>
-          <a href="#s" className={s.active}>
-            All
-          </a>
-        </div>
-        <div className={s.box}>
-          <a href="#s" className={s.app}>
-            Site For Polisan
-          </a>
-        </div>
-        <div className={s.box}>
-          <a href="#s" className={s.app}>
-            Count Timer
-          </a>
-        </div>
-        <div className={s.box}>
-          <a href="#s" className={s.app}>
-            Social App
-          </a>
-        </div>
-      </div>
-      <Parallax />
+      <Tabs>
+        <TabList className={s.tabList}>
+          <Tab>All</Tab>
+          <Tab>Site For Polisan</Tab>
+          <Tab>Count Timer</Tab>
+          <Tab>Social App</Tab>
+        </TabList>
+
+        <TabPanel>
+          <Parallax />
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 2</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 3</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 4</h2>
+        </TabPanel>
+      </Tabs>
     </div>
   );
 };
