e71f10be9f2932681f2728c489e39ed74e5f7707
ValentinLugovov <valentinlugovov@gmail.com>
2022-08-29 08:43:43 +0300

    Adds classNames for all links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,27 +4,39 @@ import logo from "../img/logo.png";
 
 const Navbar = (props) => {
   return (
-    <div className={s.nav}>
-      <div className={s.box1}>
-        <img src={logo} alt="Logo"></img>
-      </div>
-      <div className={s.box2}>
-        <a href="#hero">Home</a>
-      </div>
-      <div className={s.box3}>
-        <a href="#about">About</a>
-      </div>
-      <div className={s.box4}>
-        <a href="#skills">Skills</a>
-      </div>
-      <div className={s.box5}>
-        <a href="#project">Portfolio</a>
-      </div>
-      <div className={s.box6}>
-        <a href="#contact">Contact</a>
-      </div>
-      <div className={s.box7}>
-        <button className={s.btn}>Hire Me</button>
+    <div className={s.container}>
+      <div className={s.nav}>
+        <div className={s.box1}>
+          <img src={logo} alt="Logo"></img>
+        </div>
+        <div className={s.box2}>
+          <a href="#top" className={s.a}>
+            Home
+          </a>
+        </div>
+        <div className={s.box3}>
+          <a href="#about" className={s.a}>
+            About
+          </a>
+        </div>
+        <div className={s.box4}>
+          <a href="#skills" className={s.a}>
+            Skills
+          </a>
+        </div>
+        <div className={s.box5}>
+          <a href="#project" className={s.a}>
+            Portfolio
+          </a>
+        </div>
+        <div className={s.box6}>
+          <a href="#contact" className={s.a}>
+            Contact
+          </a>
+        </div>
+        <div className={s.box7}>
+          <button className={s.btn}>Hire Me</button>
+        </div>
       </div>
     </div>
   );
