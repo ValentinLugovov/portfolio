473930f075c85a52dea0cf0562575ecd8292be74
ValentinLugovov <valentinlugovov@gmail.com>
2022-08-29 18:31:09 +0300

    Adds link in contacts

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -11,23 +11,22 @@ const Contacts = (props) => {
         </div>
         <div className={s.box2}>
           <p className={s.header}>Useful Links</p>
-          <div className={s.links}>
-            <p>Home</p>
-            <p>About</p>
-            <p>Skills</p>
-            <p>Partfolio</p>
-            <p>Contact</p>
+          <div className={s.link}>
+            <a className={s.links} href="#top">Home</a><br/><br/>
+            <a className={s.links} href="#about">About</a><br/><br/>
+            <a className={s.links} href="#skills">Skills</a><br/><br/>
+            <a className={s.links} href="#project">Partfolio</a><br/><br/>
+            <a className={s.links} href="#contact">Contact</a>
           </div>
         </div>
         <div className={s.box3}>
           <p className={s.header}>Social Media</p>
-          <div className={s.links}>
-            <p>Instagram</p>
-            <p>Linked In</p>
-            <p>Telegram</p>
-            <p>Git Hub</p>
-            <p>Mobile</p>
-            <p>E-Mail</p>
+          <div className={s.link}>
+            <a className={s.links} href="https://www.linkedin.com/in/valentin-lugovov/">Linked In</a><br/><br/>
+            <a className={s.links} href="https://www.telegram.me/valentin_lugovov/">Telegram</a><br/><br/>
+            <a className={s.links} href="https://github.com/ValentinLugovov">Git Hub</a><br/><br/>
+            <a className={s.links} href="">Mobile</a><br/><br/>
+            <a className={s.links} href="">E-Mail</a>
           </div>
         </div>
         <div className={s.box4}></div>
