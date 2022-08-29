dcb5c274a7c9beac8a32961fecf473faff14945d
ValentinLugovov <valentinlugovov@gmail.com>
2022-08-29 08:28:07 +0300

    Renames classNames

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,14 +5,14 @@ import avatar from "../img/avatar.png";
 const Hero = (props) => {
   return (
     <div id="hero" className={s.hero}>
-      <div className={s.one}>
+      <div className={s.name}>
         Hi, I Am <span>Valentin Lugovov</span>👋
       </div>
-      <div className={s.two}>Junior Front-End Developer</div>
-      <div className={s.three}>
+      <div className={s.who}>Junior Front-End Developer</div>
+      <div className={s.phrase}>
         I design and code beautifully simple things, and I love what I do.
       </div>
-      <div className={s.four}>
+      <div className={s.photo}>
         <img src={avatar} alt="Avatar"></img>
       </div>
     </div>
