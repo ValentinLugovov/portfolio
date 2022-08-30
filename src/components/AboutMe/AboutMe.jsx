93733740162c1b72b7b34c15ed0ce955383d900a
ValentinLugovov <valentinlugovov@gmail.com>
2022-08-30 19:12:49 +0300

    Changes text about me and photo

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,25 +1,26 @@
 import React from "react";
 import s from "./aboutMe.module.css";
-import photo from "../img/photo.png";
 import btn from "../Navbar/navbar.module.css";
+import me from "../img/me.png";
 
 const AboutMe = (props) => {
   return (
     <div id="about" className={s.aboutMe}>
       <div className={s.photo}>
-        <img src={photo} alt="UserPhoto"></img>
+        <img src={me} alt="UserPhoto" className={s.me}></img>
       </div>
       <div>
         <div className={s.who}>Who I am</div>
         <div className={s.about}>About Me</div>
         <div className={s.text}>
-          Hi! I’m Kartik Bansal, and I’m a designer & developer who has passion
-          for building clean web applications with intuitive functionality. I
-          enjoy the process of turning ideas into reality using creative
-          solutions. I’m always curious about learning new skills, tools, and
-          concepts. In addition to working on various solo full stack projects,
-          I have worked with creative teams, which involves daily stand-ups and
-          communications, source control, and project management.
+          Привет! Я начинающий фронтенд разработчик, который находится в начале
+          своего пути. Начал я своё знакомство с JavaScript, с книг портала
+          code.mu "Основной учебник" и "Высший учебник". Сейчас черпаю
+          информацию из YouTube, Habr, StackOverFlow, а так же из документаций.
+          Работаю над собственным проектом - веб приложение для заполнения и
+          хранения документов в организации, можно сказать CRM'ка в миниатюре.
+          Начинал его на нативном JS, но постепенно перевожу его на React. Также
+          Тут можно найти другие мои "более стандартные" работы для портфолио.
         </div>
         <div className={s.btn}>
           <button className={btn.btn}>Hire Me</button>
