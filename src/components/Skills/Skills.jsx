67911a5e4ea1b4394bb22af7c01243f782f26cca
ValentinLugovov <valentinlugovov@gmail.com>
2022-09-01 01:46:29 +0300

    Refactoring Transfers data in another component

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,12 +1,7 @@
 import React from "react";
 import s from "./skills.module.css";
 import n from "../Navbar/navbar.module.css";
-import js from "../img/icons/js.png";
-import css from "../img/icons/css.png";
-import git from "../img/icons/git.png";
-import html from "../img/icons/html.png";
-import react from "../img/icons/react.png";
-import redux from "../img/icons/redux.png";
+import Items from "./Items/Items";
 
 const Skills = (props) => {
   return (
@@ -15,35 +10,10 @@ const Skills = (props) => {
       <div className={s.two}>What My Programming Skills Included?</div>
 
       <div className={s.three}>
-        I develop simple, intuitive and responsive user interface that helps
-        users get things done with less effort and time with those technologies.
+        Активно потребляю новую информацию по данным технологиям + изучаю новые
+        библиотеки
       </div>
-      <div className={s.box}>
-        <div className={s.miniBox}>
-          <img src={js} alt="Ico"></img>
-        </div>
-        <div className={s.miniBox}>
-          <img src={css} alt="Ico"></img>
-        </div>
-        <div className={s.miniBox}>
-          <img src={git} alt="Ico"></img>
-        </div>
-        <div className={s.miniBox}>
-          <img src={html} alt="Ico"></img>
-        </div>
-        <div className={s.miniBox}>
-          <img src={react} alt="Ico"></img>
-        </div>
-        <div className={s.miniBox}>
-          <img src={redux} alt="Ico"></img>
-        </div>
-        <div className={s.miniBox}></div>
-        <div className={s.miniBox}></div>
-        <div className={s.miniBox}></div>
-        <div className={s.miniBox}></div>
-        <div className={s.miniBox}></div>
-        <div className={s.miniBox}></div>
-      </div>
+      <Items />
       <div>
         <button className={n.btn}>Hire Me</button>
       </div>
