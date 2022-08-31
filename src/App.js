cbaa00f037925e349195a8e3b6a1b383bdc92100
ValentinLugovov <valentinlugovov@gmail.com>
2022-09-01 02:45:08 +0300

    Adds MediaQuery in App

--- src/App.js
+++ src/App.js
@@ -5,22 +5,52 @@ import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Projects from "./components/Projects/Projects";
 import Skills from "./components/Skills/Skills";
+import { useMediaQuery } from "react-responsive";
 
 const App = () => {
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-width: 1224px)",
+  });
+  const isBigScreen = useMediaQuery({ query: "(min-width: 1824px)" });
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
+  const isPortrait = useMediaQuery({ query: "(orientation: portrait)" });
+  const isRetina = useMediaQuery({ query: "(min-resolution: 2dppx)" });
 
   return (
-    <div className="App">
-      <a href="#top" className="to-up">
-        {" "}
-      </a>
-      <Navbar />
-      <Hero />
-      <AboutMe />
-      <Skills />
-      <Projects />
-      <Contacts />
+    <div>
+      {isDesktopOrLaptop && (
+        <div className="App">
+          <a href="#top" className="to-up">
+            {" "}
+          </a>
+          <Navbar />
+          <Hero />
+          <AboutMe />
+          <Skills />
+          <Projects />
+          <Contacts />
+        </div>
+      )}
+      {isTabletOrMobile && (
+        <p className="sorry">
+          Sorry, I didn 't have time to make an adaptive website for you
+        </p>
+      )}
+      {isTabletOrMobile && (
+        <div className="App">
+          <a href="#top" className="to-up">
+            {" "}
+          </a>
+          <Navbar />
+          <Hero />
+          <AboutMe />
+          <Skills />
+          <Projects />
+          <Contacts />
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
