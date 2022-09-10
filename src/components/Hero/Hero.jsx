8a48aa41a3fbbec527c4fdbd86be4e83f73f5376
Valentin <valentinlugovov@gmail.com>
2022-09-10 23:32:34 +0300

    Try deploy in heroku

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import s from "./hero.module.css";
-import avatar from "../img/avatar.PNG";
+// import avatar from "../img/avatar.PNG";
 
 const Hero = (props) => {
   return (
     <div id="hero" className={s.hero}>
-     <div className={s.photo}>
-        <img src={avatar} alt="Avatar"></img>
-      </div>
       <div className={s.name}>
         Hi, I Am <span>Valentin Lugovov</span>👋
       </div>
@@ -15,7 +12,9 @@ const Hero = (props) => {
       <div className={s.phrase}>
         I design and code beautifully simple things, and I love what I do.
       </div>
-     
+      {/* <div className={s.photo}>
+        <img src={avatar} alt="Avatar"></img>
+      </div> */}
     </div>
   );
 };
